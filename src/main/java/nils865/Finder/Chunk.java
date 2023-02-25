d3be6c660213074a9ba18ca5c202113ce6bb49c8
Nils <nils.staas0806@gmail.com>
2023-02-25 16:25:13 +0100

    added check for slimechunk

--- src/main/java/nils865/Finder/Chunk.java
+++ src/main/java/nils865/Finder/Chunk.java
@@ -1,11 +1,26 @@
 package nils865.Finder;
 
+import java.util.Random;
+
 public class Chunk {
     private final int x;
-    private final int y;
+    private final int z;
+    private final boolean slimeChunk;
 
-    public Chunk(int x, int y) {
+    public Chunk(int x, int z) {
         this.x = x;
-        this.y = y;
+        this.z = z;
+
+        this.slimeChunk = isSlimeChunk(12345L);
+    }
+
+    public boolean isSlimeChunk(long seed) {
+        Random rnd = new Random(seed +
+                (long) (this.x * this.x * 0x4c1906) +
+                (long) (this.x * 0x5ac0db) +
+                (long) (this.z * this.z) * 0x4307a7L +
+                (long) (this.z * 0x5f24f) ^ 0x3ad8025f);
+    
+        return rnd.nextInt(10) == 0;
     }
 }
