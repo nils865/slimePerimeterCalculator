9377074bac8f4d21c3496b76d4b4cd48d18b3f46
Nils <nils.staas0806@gmail.com>
2023-02-25 16:31:29 +0100

    added system arguments

--- src/main/java/nils865/Main.java
+++ src/main/java/nils865/Main.java
@@ -2,6 +2,17 @@ package nils865;
 
 public class Main {
     public static void main(String[] args) {
+        for (String arg : args) {
+            switch (arg) {
+                case "debug":
+                    break;
+            
+                default:
+                    System.out.println("Argument \"" + arg + "\" not found");
+                    return;
+            }
+        }
+
         System.out.println("Hello World!");
     }
 }
