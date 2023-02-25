65b7e181fbc9d7a721223a1238f73621341c2310
Nils <nils.staas0806@gmail.com>
2023-02-25 16:22:35 +0100

    added chunk class

---
+++ src/main/java/nils865/Finder/Chunk.java
@@ -0,0 +1,11 @@
+package nils865.Finder;
+
+public class Chunk {
+    private final int x;
+    private final int y;
+
+    public Chunk(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
